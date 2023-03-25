0a2d148e2bbe0aa346fc3eb736bb5eb55b222933
Виктор Рунтов <runtovvs@gmail.com>
2023-03-25 17:52:28 +0300

    изменил проверку с if else на проверку через assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,14 @@
+import { strict as assert} from 'node:assert';
 import capitalize from "../capitalize.js";
 
+assert.equal(capitalize('hello'), 'Hello');
+// if (capitalize("hello") !== "Hello") {
+//   throw new Error("Функция работает неверно!");
+// }
 
-if (capitalize("hello") !== "Hello") {
-  throw new Error("Функция работает неверно!");
-}
-
-if (capitalize("") !== "") {
-  throw new Error("Функция работает неверно!");
-}
+assert.equal(capitalize(''), '');
+// if (capitalize("") !== "") {
+//   throw new Error("Функция работает неверно!");
+// }
 
 console.log("Все тесты пройдены!");
